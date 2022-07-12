f01a74cd0483fcee19b30176d822c5a26df334b9
Evgenii <vasilkalin@gmail.com>
2022-07-12 17:07:30 +0300

    App works, but not correct

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LayoutFour from "./components/Layout/LayoutFour";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LayoutFour />
+
   </React.StrictMode>
 );
 
@@ -15,3 +15,17 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+// import LayoutFour from "./components/Layout/LayoutFour";
+// import {connect} from "react-redux";
+//
+// const Home = () => {
+//     return <LayoutFour />
+//
+// }
+//
+// export default connect(Home);
